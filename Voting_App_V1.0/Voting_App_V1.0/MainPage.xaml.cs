0b365ca6c79ff864c0a4c8ad49a781a8c10f67ad
Samuel <sammys.email.home@gmail.com>
2022-11-30 17:03:16 -0600

    Add project files.

---
+++ Voting_App_V1.0/Voting_App_V1.0/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Voting_App_V1._0
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void InputView_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        private void Entry_OnCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListViewPage1());
+        }
+    }
+}
